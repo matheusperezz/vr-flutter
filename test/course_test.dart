2a077e2924df83e3f9401519bf0097e3d1bd9f71
matheusperezz <matheus.mperez08@gmail.com>
2024-01-29 16:09:10 -0300

    filter working on courses and students screen

---
+++ test/course_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:vr_application/_core/routes_names.dart';
+import 'package:vr_application/main.dart';
+import 'package:vr_application/modules/app_module.dart';
+
+void main() {
+
+}
